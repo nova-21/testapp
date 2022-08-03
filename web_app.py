cc94df1bfdf20fb6f6b466f40adb56efd1a790fb
nova-21 <44357843+nova-21@users.noreply.github.com>
2022-08-03 16:33:26 -0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -2,20 +2,26 @@ from flask import Flask
 import py_eureka_client.eureka_client as eureka_client
 from py_eureka_client import netint_utils
 
+estados={"sala":"","dormitorio":"","cocina":""}
 rest_port =80
-ip = netint_utils.get_first_non_loopback_ip("192.168.10.0/24")
-host="iluminacion-webavanzada.herokuapp.com"
-eureka_client.init(eureka_server="http://52.73.98.2:8099/eureka",
-                   app_name="iluminacion", instance_port=rest_port, instance_ip=ip, instance_host=host)
+
 
 app = Flask(__name__)
 
-@app.post("/iluminacion/<lumens>/<hora>")
-def iluminacion(lumens,hora):
+@app.post("/iluminacion/<habitacion>/<lumens>/<hora>")
+def iluminacion(habitacion,lumens,hora):
     if int(lumens) < 15000:
         if (int(hora) >= 18 or int(hora)<=7):
-            return "Luces encendidas"
+            estados[habitacion]="Luces encendidas"
+            print(estados)
+            return estados
         else:
-            return "Persianas abiertas"
+            estados[habitacion] = "Persianas abiertas"
+            return estados
     else:
-        return "Apagadas/Cerradas"
+        estados[habitacion] = "Apagadas/Cerradas"
+        return estados
+
+@app.get("/iluminacion")
+def getIluminacion():
+    return estados
