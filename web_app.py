9dfb794079afff620e9f9ffd30e0a47738fc9810
nova-21 <44357843+nova-21@users.noreply.github.com>
2022-08-03 16:58:55 -0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -5,6 +5,9 @@ from py_eureka_client import netint_utils
 estados={"sala":"Apagadas/Cerradas","dormitorio":"Apagadas/Cerradas","cocina":"Apagadas/Cerradas"}
 rest_port =80
 
+host="iluminacion-webavanzada.herokuapp.com"
+eureka_client.init(eureka_server="http://52.73.98.2:8099/eureka",
+                   app_name="iluminacion", instance_port=rest_port,instance_host=host)
 
 app = Flask(__name__)
 
