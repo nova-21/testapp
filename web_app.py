1b75af2a7ca6e980a5d0d866362dacb4bb3a67d8
nova-21 <44357843+nova-21@users.noreply.github.com>
2022-08-03 20:08:31 -0500

    1

--- web_app.py
+++ web_app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import py_eureka_client.eureka_client as eureka_client
-from pyctuator.auth import BasicAuth
 from pyctuator.pyctuator import Pyctuator
 
 estados={"sala":"Apagadas/Cerradas","dormitorio":"Apagadas/Cerradas","cocina":"Apagadas/Cerradas"}
@@ -8,9 +7,6 @@ rest_port =80
 
 host="iluminacion-webavanzada.herokuapp.com"
 app_name="iluminacion"
-
-eureka_client.init(eureka_server="http://52.73.98.2:8099/eureka",
-                   app_name=app_name, instance_port=rest_port,instance_host=host)
 
 app = Flask(app_name)
 
@@ -32,12 +28,13 @@ def iluminacion(habitacion,lumens,hora):
 def getIluminacion():
     return estados
 
-auth = BasicAuth("kalkstein", "th3_eth3r")
+eureka_client.init(eureka_server="http://52.73.98.2:8099/eureka",
+                   app_name=app_name, instance_port=rest_port,instance_host=host)
+
 Pyctuator(
     app,
     app_name,
-    app_url=host,
+    app_url="http://iluminacion-webavanzada.herokuapp.com",
     pyctuator_endpoint_url="http://iluminacion-webavanzada.herokuapp.com/pyctuator",
-    registration_url="http://34.232.227.255:8086",
-    registration_auth=auth
+    registration_url="http://34.232.227.255:8086/instances"
 )
