c8e29fc7ada9989e42333fa84feb5cc55dc6d99d
nova-21 <44357843+nova-21@users.noreply.github.com>
2022-08-03 19:01:39 -0500

    te

--- web_app.py
+++ web_app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 import py_eureka_client.eureka_client as eureka_client
 from py_eureka_client import netint_utils
+from pyctuator.pyctuator import Pyctuator
 
 estados={"sala":"Apagadas/Cerradas","dormitorio":"Apagadas/Cerradas","cocina":"Apagadas/Cerradas"}
 rest_port =80
 
 host="iluminacion-webavanzada.herokuapp.com"
+app_name="iluminacion"
+
 eureka_client.init(eureka_server="http://52.73.98.2:8099/eureka",
-                   app_name="iluminacion", instance_port=rest_port,instance_host=host)
+                   app_name=app_name, instance_port=rest_port,instance_host=host)
 
 app = Flask(__name__)
 
@@ -28,3 +31,11 @@ def iluminacion(habitacion,lumens,hora):
 @app.get("/iluminacion")
 def getIluminacion():
     return estados
+
+Pyctuator(
+    app,
+    app_name=app_name,
+    app_url="http://example-app.com",
+    pyctuator_endpoint_url="http://example-app.com/pyctuator",
+    registration_url=os.getenv("SPRING_BOOT_ADMIN_URL")
+)
